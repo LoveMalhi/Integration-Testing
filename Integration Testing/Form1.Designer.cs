d0324f604975def4b53e53529ebc530cbcfc9854
LoveMalhi <Lkaur6201@2A206-A06.conestogac.on.ca>
2015-11-05 15:26:03 -0500

    Commit last
    Done

--- Integration Testing/Form1.Designer.cs
+++ Integration Testing/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.tb1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tb1
             // 
-            this.tb1.Location = new System.Drawing.Point(128, 140);
+            this.tb1.Location = new System.Drawing.Point(143, 45);
             this.tb1.Name = "tb1";
             this.tb1.Size = new System.Drawing.Size(100, 20);
             this.tb1.TabIndex = 0;
@@ -44,37 +46,55 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 44);
+            this.label1.Location = new System.Drawing.Point(24, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Enter Birth date";
             // 
-            // dateTimePicker1
+            // button1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(107, 44);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(210, 20);
-            this.dateTimePicker1.TabIndex = 3;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.button1.Location = new System.Drawing.Point(123, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(123, 206);
+            this.label2.Location = new System.Drawing.Point(58, 203);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 29);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 342);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tb1);
             this.Name = "Form1";
@@ -89,8 +109,10 @@
 
         private System.Windows.Forms.TextBox tb1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,31 +1,65 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Integration_Testing;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+
+
         /* 1.
-         * Test Identifier:0.
-         * Test Description:  age=18
+         * Test Identifier:1999.
+         * Test Description:  Find whether age is equal to 16
          * Method of Execution: Automated.
-         * Input Data: .
-         * Expected Output: 18.
-         * Observed Output:  Test passed.
+         * Input Data:2015-1999 .
+         * Expected Output: 16.
+         * Observed Output:  Test passed,Alcohol is not allowed in Germany.
          * Actions:nothing
          */
-
-        public void age_equal()
+        [TestMethod]
+        public void AgeisEqual()
         {
-             int i = 2015;
-            int Age =1997;
-            int result =
-            Assert.AreEqual(50, result, "Test Passed");
+            int tdy = 2015;
+            int dob = 1999;
+            int result = Integration_Testing.Form1.findage(tdy, dob);
+            Assert.AreEqual(16, result, "Alcohol is not allowed in Germany");
         }
+        /* 2.
+        * Test Identifier:1979.
+        * Test Description: Find whether age is grater than 16 
+        * Method of Execution: Automated.
+        * Input Data: .
+        * Expected Output: 36
+        * Observed Output:  Test passed,Alcohol is Allowed in Germany.
+        * Actions:nothing
+        */
+        [TestMethod]
+        public void Ageisgreater()
+        {
+            int tdy = 2015;
+            int dob = 1979;
+            int result = Integration_Testing.Form1.findage(tdy, dob);
+            Assert.AreEqual(36, result, "Alcohol is Allowed in Germany");
+        }
+        /* 3.
+        * Test Identifier:1998
+        * Test Description:  Find whether age is smaller than 16  
+         
+        * Method of Execution: Automated.
+        * Input Data: .
+        * Expected Output: 14.
+        * Observed Output:  Test failed.
+        * Actions:Change the expected value to 17
+        */
+        [TestMethod]
+        public void Ageissmaller()
+        {
+            int tdy = 2015;
+            int dob = 1998;
+            int result = Integration_Testing.Form1.findage(tdy, dob);
+            Assert.AreEqual(14, result, "Alcohol is not allowed in Germany");
         }
     }
 }
